77110938a6703f6b579df0aa4a005376829ea132
Dmitriy Shishkin <6ast3r@gmail.com>
2021-08-06 18:35:41 +0700

    Repro modal issue

--- App.js
+++ App.js
@@ -7,43 +7,142 @@ import {
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { TransitionPresets, createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Home = ({
   navigation,
 }) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Button
-      title="Navigate to Settings screen"
+      title="Navigate to Intermediate screen"
       onPress={() => {
-        navigation.navigate('Settings');
+        navigation.navigate('Intermediate');
       }}
     />
   </View>
 );
 
-const Settings = () => (
+const Intermediate = ({
+  navigation,
+}) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Settings screen</Text>
+    <Button
+      title="Navigate to Modal screen"
+      onPress={() => {
+        navigation.navigate('Modal');
+      }}
+    />
   </View>
 );
+
+const Modal = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Modal screen</Text>
+  </View>
+);
+
+const StackScreen = () => (
+  <Stack.Navigator
+    screenOptions={{
+      detachPreviousScreen: false,
+      headerStyleInterpolator: forFade,
+      headerStyle: {
+        shadowColor: 'transparent',
+        elevation: 0,
+        backgroundColor: 'gray',
+      },
+      cardStyle: {
+        backgroundColor: 'gray',
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Home"
+      options={{
+        headerLeft: () => <View />,
+        headerRight: () => <View />,
+      }}
+      component={Home}
+    />
+    <Stack.Screen
+      name="Intermediate"
+      options={{
+        headerLeft: () => <View />,
+        headerRight: () => <View />,
+      }}
+      component={Intermediate}
+    />
+    <Stack.Screen
+      name="Modal"
+      options={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'green',
+        },
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }}
+      component={Modal}
+    />
+  </Stack.Navigator>
+);
+
+const TabsScreen = () => (
+  <Tab.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <Tab.Screen name="StackScreen" component={StackScreen} />
+  </Tab.Navigator>
+);
+
+const forFade = ({ current }: StackHeaderInterpolationProps): StackHeaderInterpolatedStyle => {
+  const opacity = current.progress;
+
+  return {
+    leftButtonStyle: { opacity },
+    rightButtonStyle: { opacity },
+    titleStyle: { opacity },
+    backgroundStyle: { opacity },
+  };
+};
+
+const blueOverlay = <View style={{ flex: 1, backgroundColor: 'blue' }} />;
+
+const renderBlueOverlay = () => blueOverlay;
 
 function MyStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <MainStack.Navigator
         screenOptions={{
-          headerStyle: { backgroundColor: 'gray' },
+          detachPreviousScreen: false,
+          headerStyleInterpolator: forFade,
+          headerStyle: {
+            shadowColor: 'transparent',
+            elevation: 0,
+            backgroundColor: 'blue',
+          },
+          cardStyle: {
+            backgroundColor: 'blue',
+          },
         }}
       >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Settings"
-          options={TransitionPresets.ModalTransition}
-          component={Settings}
+        <Tab.Screen
+          name="TabsScreen"
+          options={{
+            ...TransitionPresets.ScaleFromCenterAndroid,
+            headerShown: false,
+            cardOverlayEnabled: true,
+            cardOverlay: renderBlueOverlay,
+          }}
+          component={TabsScreen}
         />
-      </Stack.Navigator>
+      </MainStack.Navigator>
     </NavigationContainer>
   );
 }
